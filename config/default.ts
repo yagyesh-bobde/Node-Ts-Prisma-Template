b7a56c6fb575270b8477fdec95227d582158369a
Yagyesh Bobde <yagyeshSRbobde@gmail.com>
2024-03-23 19:02:59 +0530

    added dev script and updated a basic express server with config

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 3000, 
+    DB_URL: "mongodb://localhost:27017/express-mongo"
+}
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+require('dotenv').config()
+import express from 'express'
+import config from 'config'
+
+
+const app = express()
+
+const port = config.get("port");
+
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});

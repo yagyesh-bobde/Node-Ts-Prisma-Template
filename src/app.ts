8ae338380585338e3b2d94ee9b0af66179556cc4
Yagyesh Bobde <yagyeshSRbobde@gmail.com>
2024-03-23 19:46:37 +0530

    updated prisma client

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import config from 'config'
 import log from './utils/logger';
 import router from './routes';
+import { PrismaClient } from '@prisma/client';
 
 
 const app = express()
@@ -14,6 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router)
+
+
+export const prismaClient = new PrismaClient({
+    log: ['query', 'info', 'warn'],
+});
 
 app.listen(port, () => {
     log.info(`Server started on http://localhost:${port}`);

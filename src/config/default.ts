c5f392cc89a7f55fa415dafe909b9ad00b34d6e9
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2024-03-24 13:12:13 +0530

    Update default.ts

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,12 +3,12 @@ const SALT_ROUNDS = parseInt(`${process.env.SALT_ROUNDS}`) || 10;
 const MIN_PASSWORD_LENGTH = parseInt(`${process.env.MIN_PASSWORD_LENGTH}`) || 6;
 const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID!;
 const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET!;
-
+const DB_URL = process.env.DB_URL!;
 
 
 export default {
     port: 3000, 
-    DB_URL: "mongodb+srv://yagyesh:yagyesh@developers-zone.69fu5rm.mongodb.net", 
+    DB_URL: DB_URL, 
     logLevel: "info", 
     JWT_SECRET, 
     SALT_ROUNDS,
